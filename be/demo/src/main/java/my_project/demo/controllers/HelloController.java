c2414561f1397f6cd31c0deb2d938832c68d9c1e
MK <qscvdefb@gmail.com>
2024-07-23 10:10:26 +0700

    rest controller

---
+++ be/demo/src/main/java/my_project/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package my_project.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String home() {
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
